169fac66064ad0e184bf065488dd2484e40c1d74
unknown <Okbet@OKBET-CAPL032.kingwinventures.com>
2023-12-15 01:28:34 +0800

    Initial commit

---
+++ app/src/main/java/com/ceej/expensetracker/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ceej.expensetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ceej.expensetracker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivity: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainActivity = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivity.root)
+    }
+
+}
